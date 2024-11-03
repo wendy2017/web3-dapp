57b1dc19f9d11c254f803af4cee624d18a5d5e5a
wendy <lilinvv@163.com>
2024-11-03 23:21:46 +0800

    chore(*): fix build fail

--- src/app/web3/page.tsx
+++ src/app/web3/page.tsx
@@ -1,15 +1,8 @@
-import Link from "next/link";
-import Image from "next/image";
-import { useAccount } from "wagmi";
+"use client";
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-// import Factory from "./nftMarketplace/create";
 import Factory from "./nftMarketplace/create/page";
 
 const Web3 = () => {
-  const { isConnected } = useAccount();
-  console.log("isConnected==", isConnected);
-
   return (
     <div className="flex flex-col gap-4">
       {/* 默认展示nft market */}
